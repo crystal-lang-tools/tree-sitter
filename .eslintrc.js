06d520247e62fadee8450f6adfee383f98715795
devnote-dev <devnote.dev75@gmail.com>
2023-08-28 02:10:21 +0100

    chore: add eslint dependency & config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'max-len': [
+      'error',
+      {
+        code: 160,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+      },
+    ],
+    'one-var': ['error', 'consecutive'],
+  },
+};
